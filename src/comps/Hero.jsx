ade0eb936f8c71cf134cb0b646813d8c0e9f7262
j-o-phillips <jphillips105@googlemail.com>
2023-12-11 10:49:57 +0100

    camera pos test

--- src/comps/Hero.jsx
+++ src/comps/Hero.jsx
@@ -8,18 +8,18 @@ const Hero = () => {
     <>
       <div className="h-screen  ">
         <div
-          className="text-gray-500 text-5xl absolute m-4 left-4 top-16 md:text-6xl md:left-20 md:top-16 "
+          className="text-gray-300 text-5xl absolute m-4 left-4 top-16 md:text-6xl md:left-20 md:top-16 "
           style={{ fontFamily: "Open Sans, sans serif" }}
         >
           <h1>Jake Phillips</h1>
         </div>
         <div
-          className="text-gray-500 text-4xl absolute m-4 right-4 bottom-20 sub-title md:text-5xl md:right-20"
+          className="text-gray-400 text-4xl absolute m-4 left-8 top-32 sub-title md:text-5xl md:left-28"
           style={{ fontFamily: "Open Sans, sans serif" }}
         >
           <h1>Developer</h1>
         </div>
-        <Canvas camera={{ position: [-10, 0, 0], fov: 25, near: 5, far: 20 }}>
+        <Canvas camera={{ position: [-20, 0, 0], fov: 25, near: 5, far: 20 }}>
           <Blob />
         </Canvas>
       </div>
